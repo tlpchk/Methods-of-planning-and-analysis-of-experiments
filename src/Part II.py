2c8096998a99bda678b72fa4f8cab2fc922fa9aa
tlpchk <max.telepchuk@gmail.com>
2020-05-31 13:15:35 +0200

    task 2.5 : code

--- src/Part II.py
+++ src/Part II.py
@@ -3,7 +3,7 @@ from scipy.stats import t, norm
 
 
 def t3():
-    print('3')
+    print('# 3')
     X = [435, 533, 393, 458, 525, 481, 324, 437, 348, 503, 383, 395, 416, 553, 500, 488]
     sigma = 70
     alpha = 0.01
@@ -14,11 +14,12 @@ def t3():
     N = norm(0, 1)
     L = x_m - N.ppf(1 - alpha / 2) * sigma / np.sqrt(n)
     P = x_m + N.ppf(1 - alpha / 2) * sigma / np.sqrt(n)
-    print(f'[{L},{P}]')
+    print(f'[L,P] = [{x_m} - {N.ppf(1 - alpha / 2)} * {sigma} / {np.sqrt(n)},'
+          f' {x_m} + {N.ppf(1 - alpha / 2)} * {sigma} / {np.sqrt(n)}] = [{L},{P}]')
 
 
 def t4():
-    print('4')
+    print('# 4')
     X = np.array([383, 284, 339, 340, 305, 386, 378, 335, 344, 346])
     alpha1 = 0.1
     alpha2 = 0.05
@@ -29,12 +30,30 @@ def t4():
 
     L1 = x_m - t.ppf(1 - alpha1 / 2, n - 1) * S / np.sqrt(n)
     P1 = x_m + t.ppf(1 - alpha1 / 2, n - 1) * S / np.sqrt(n)
-    print(f'1-a = {1 - alpha1} => [L,P] = [{L1},{P1}]')
+    print(f'1-a = {1 - alpha1} => '
+          f'[L,P] = [{x_m} - {t.ppf(1 - alpha1 / 2, n - 1)} * {S} / {np.sqrt(n)},'
+                  f' {x_m} + {t.ppf(1 - alpha1 / 2, n - 1)} * {S} / {np.sqrt(n)}] = [{L1},{P1}]')
 
     L2 = x_m - t.ppf(1 - alpha2 / 2, n - 1) * S / np.sqrt(n)
     P2 = x_m + t.ppf(1 - alpha2 / 2, n - 1) * S / np.sqrt(n)
-    print(f'1-a = {1 - alpha2} => [L,P] = [{L2},{P2}]')
+    print(f'1-a = {1 - alpha2} => '
+          f'[L,P] = [{x_m} - {t.ppf(1 - alpha2 / 2, n - 1)} * {S} / {np.sqrt(n)},'
+          f' {x_m} + {t.ppf(1 - alpha2 / 2, n - 1)} * {S} / {np.sqrt(n)}] = [{L2},{P2}]')
+
+
+def t5():
+    print('# 5')
+    alpha = 0.05
+
+    x_m = 280
+    n = 200
+    S = 160
+
+    L = x_m - norm.ppf(1 - alpha / 2) * S / np.sqrt(n)
+    P = x_m + norm.ppf(1 - alpha / 2) * S / np.sqrt(n)
+    print(f'[L,P] = [280 - 1.96 * 160 / 14.14, 280 + 1.96 * 160 / 14.14] = [{L}, {P}]')
 
 
 t3()
 t4()
+t5()
