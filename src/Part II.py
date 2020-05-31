1fd3c53283a67a1456eca41ed142deb72a7e2800
tlpchk <max.telepchuk@gmail.com>
2020-05-31 14:11:45 +0200

    tasks 2.7, 2.8 : code

--- src/Part II.py
+++ src/Part II.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.stats import t, norm
+from scipy.stats import t, norm, chi2
 
 
 def t3():
@@ -32,7 +32,7 @@ def t4():
     P1 = x_m + t.ppf(1 - alpha1 / 2, n - 1) * S / np.sqrt(n)
     print(f'1-a = {1 - alpha1} => '
           f'[L,P] = [{x_m} - {t.ppf(1 - alpha1 / 2, n - 1)} * {S} / {np.sqrt(n)},'
-                  f' {x_m} + {t.ppf(1 - alpha1 / 2, n - 1)} * {S} / {np.sqrt(n)}] = [{L1},{P1}]')
+          f' {x_m} + {t.ppf(1 - alpha1 / 2, n - 1)} * {S} / {np.sqrt(n)}] = [{L1},{P1}]')
 
     L2 = x_m - t.ppf(1 - alpha2 / 2, n - 1) * S / np.sqrt(n)
     P2 = x_m + t.ppf(1 - alpha2 / 2, n - 1) * S / np.sqrt(n)
@@ -54,6 +54,37 @@ def t5():
     print(f'[L,P] = [280 - 1.96 * 160 / 14.14, 280 + 1.96 * 160 / 14.14] = [{L}, {P}]')
 
 
+def t7():
+    print('# 7')
+
+    a = 0.1
+    x_m = 20.2
+    n = 10
+    S2 = 0.96
+
+    L = (n - 1) * S2 / chi2.ppf(1 - a / 2, n - 1)
+    P = (n - 1) * S2 / chi2.ppf(a / 2, n - 1)
+    print(f'[L,P] = [{n - 1} * {S2} / {chi2.ppf(1 - a / 2, n - 1)},'
+          f' {n - 1} * {S2} / {chi2.ppf(a / 2, n - 1)}] = [{L},{P}]')
+
+
+def t8():
+    print('# 8')
+    X = [102, 105, 102, 100, 108, 97, 96, 98, 100, 101]
+    a = 0.05
+
+    x_m = np.mean(X)
+    n = len(X)
+    S = np.sqrt(1 / (n - 1) * np.sum((X - x_m) ** 2))
+
+    L = np.sqrt((n - 1) * S ** 2 / chi2.ppf(1 - a / 2, n - 1))
+    P = np.sqrt((n - 1) * S ** 2 / chi2.ppf(a / 2, n - 1))
+    print(f'[L,P] = [sqrt({n - 1} * {S ** 2} / {chi2.ppf(1 - a / 2, n - 1)}),'
+          f' sqrt({n - 1} * {S ** 2} / {chi2.ppf(a / 2, n - 1)})] = [{L},{P}]')
+
+
 t3()
 t4()
 t5()
+t7()
+t8()
