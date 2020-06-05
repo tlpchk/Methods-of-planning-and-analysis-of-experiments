c0a01a8ce60d01cf9e5eead11ddaa9b69ec6fbcd
mrcell-git <marcel.cielinski@gmail.com>
2020-06-05 23:13:05 +0200

    task 1_8 and 2_5

--- src/Part I.py
+++ src/Part I.py
@@ -1,5 +1,5 @@
 import scipy.stats as stats
-from sympy.stats import Binomial, density, P, E
+from sympy.stats import Binomial, Uniform, density, P, E, variance
 
 
 def t1():
@@ -16,6 +16,18 @@ def t1():
     print('c)', ex)
 
 
+def t2():
+    print('2')
+
+
+def t3():
+    print('3')
+
+
+def t4():
+    print('4')
+
+
 def t5():
     print('5')
 
@@ -28,6 +40,7 @@ def t5():
 
     b = N.cdf(0.5)  # P( X < 0.5)
     print('a)', b)
+
 
 def t6():
     print(6)
@@ -45,6 +58,26 @@ def t6():
     a2 = b2 * sigma + mu
     print('a2)', a2)
 
+
+def t7():
+    print('7')
+
+
+def t8():
+    print('8')
+
+    X = Uniform(name='X', left=0, right=1)
+    Y = Binomial(name='Y', n=10, p=0.5) # TO JEST POLSKI BERNOULLI JBC
+
+    a = E(2*X - 3*Y)
+    b = E(X * Y)
+    c = variance((3*Y - X) / 2)
+
+    print(f'a) {a}')
+    print(f'b) {b}')
+    print(f'c) {c}')
+
+
 def t9():
     print('9')
 
@@ -56,7 +89,27 @@ def t9():
     print('a)', a)
 
 
+def t10():
+    print('10')
+
+
+print('__________________________________')
 t1()
+print('__________________________________')
+t2()
+print('__________________________________')
+t3()
+print('__________________________________')
+t4()
+print('__________________________________')
 t5()
+print('__________________________________')
 t6()
+print('__________________________________')
+t7()
+print('__________________________________')
+t8()
+print('__________________________________')
 t9()
+print('__________________________________')
+t10()
